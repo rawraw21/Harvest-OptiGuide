af348c0900dbc8d9ff4f8499bfd740447f6849bf
rawraw21 <152363340+rawraw21@users.noreply.github.com>
2024-02-06 12:02:41 +0700

    Update 2_📚_About.py

--- pages/2_📚_About.py
+++ pages/2_📚_About.py
@@ -1,16 +1,26 @@
 import streamlit as st
 from PIL import Image
 
-brown_text = '<h1 style="color:brown;">About</h1>'
+st.title("Latar Belakang Harvest-OptiGuide")
 
-st.markdown(brown_text, unsafe_allow_html=True)
 
-image_path = r'gambar.png'
+video_path = 'E:\\III RPLK\\Pemrograman Paralel\\Crop_Recommendation_System-main\\Crop_Recommendation_System-main\\video.mp4'
+
+
+
+video_file = open(video_path, 'rb')
+video_bytes = video_file.read()
+
+st.video(video_bytes)
+
+
+st.markdown("🌿 **Mengoptimalkan Pertanian dengan Aplikasi Rekomendasi Tanaman Berbasis Model Random Forest** 🌿")
+
+image_path = r'E:\III RPLK\Pemrograman Paralel\Crop_Recommendation_System-main\Crop_Recommendation_System-main\gambar.png'
 image = Image.open(image_path)
 st.image(image, use_column_width=True)
 
 st.write("""
-🌿 **Mengoptimalkan Pertanian dengan Aplikasi Rekomendasi Tanaman Berbasis Model Random Forest** 🌿
 
 Dalam dunia pertanian yang terus berkembang, teknologi semakin memainkan peran kunci untuk meningkatkan hasil panen dan efisiensi pertanian. Salah satu inovasi terbaru adalah penggunaan model Random Forest dalam Aplikasi Rekomendasi Tanaman. 📊
 
@@ -25,14 +35,16 @@ Selain dari kondisi tanah, Aplikasi Rekomendasi Tanaman juga memperhitungkan fak
 **Langkah 3: Visualisasi dengan Chart 📈**
 Untuk memberikan gambaran yang lebih jelas, aplikasi menyajikan hasilnya dalam bentuk diagram garis. 🌾 Chart ini dengan sederhana menjelaskan seberapa besar pengaruh masing-masing faktor seperti Nitrogen, Fosfor, Kalium, dan pH terhadap rekomendasi tanaman yang diberikan oleh model Random Forest.
 
-Dengan pendekatan yang inovatif ini, para petani dapat dengan mudah mengakses rekomendasi yang disesuaikan dengan kondisi tanah dan cuaca setempat. 🌍 Hasil panen yang lebih baik dan pertanian yang efisien adalah tujuan utama dari Aplikasi Rekomendasi Tanaman ini.
+Dengan pendekatan yang inovatif ini, para petani dapat dengan mudah mengakses rekomendasi yang disesuaikan dengan kondisi unik tanah dan cuaca mereka. 🌍 Hasil panen yang lebih baik dan pertanian yang efisien adalah tujuan utama dari Aplikasi Rekomendasi Tanaman ini.
 
 Jadi, mari kita bersama-sama memasuki era pertanian yang cerdas dan efisien dengan teknologi yang melayani petani dan bumi kita. 🚜✨
 """)
+
+
 
 def local_css(file_name):
     with open(file_name) as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
 
-local_css("style.css")
+local_css("E:\\III RPLK\\Pemrograman Paralel\\Crop_Recommendation_System-main\\Crop_Recommendation_System-main\\style.css")
